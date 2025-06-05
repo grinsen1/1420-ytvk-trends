060b0f204dd79f8b80f01759c4a34050039e131c
grinsen1 <grinsennsk@gmail.com>
2025-06-05 16:14:23 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1469,7 +1469,7 @@ if (analysisContainer) {
     analysisContainer.innerHTML = `
         <h5>Актуальность видео для 14-20: (${aiScore}%):</h5>
         <ul>
-            ${insights.map(insight => `<li>${insight..replace(/\*(.*?)\*/g, '<em>$1</em>').replace(/\n/g, '<br>').replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>')}</li>`).join('')}
+            ${insights.map(insight => `<li>${insight.replace(/\*(.*?)\*/g, '<em>$1</em>').replace(/\n/g, '<br>').replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>')}</li>`).join('')}
         </ul>
     `;
     analysisContainer.classList.remove('hidden');
